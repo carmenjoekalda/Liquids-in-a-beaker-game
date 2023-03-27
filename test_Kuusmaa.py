6f8c4b24af1e687812521a240b58cf33d5aba8e0
Rasmusk <rasmus.kuusmaa@voco.ee>
2023-03-27 13:50:58 +0300

    Added a white window

--- test_Kuusmaa.py
+++ test_Kuusmaa.py
@@ -1,1 +1,12 @@
-print("RasmusK")
+import pygame
+
+pygame.init()
+win = pygame.display.set_mode((900,900))
+win.fill("white")
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
