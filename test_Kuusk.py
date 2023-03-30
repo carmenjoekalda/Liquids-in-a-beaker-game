aca4c3979d84674b7008af875b0413ca4472cd8a
Sten Kuusk <sten.kuusk@voco.ee>
2023-03-30 09:36:16 +0300

    white screen simulator

--- test_Kuusk.py
+++ test_Kuusk.py
@@ -1,1 +1,12 @@
-print("Sten")
+import pygame
+
+pygame.init()
+win = pygame.display.set_mode((900,900))
+win.fill((255,255,255))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
